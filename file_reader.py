7ca5247cd3bbcc609b9214a608af17f79263787e
Reese Pelletier <reese15910@gmail.com>
2025-06-28 21:54:20 -0400

    implemented file reader module

--- file_reader.py
+++ file_reader.py
@@ -1,9 +1,18 @@
-import pymupdf # type: ignore
+import pymupdf
 
-pdf_document = pymupdf.open(r'docs\Project Spec.pdf') # Open the PDF file
+def read_pdf(pdf_path):
+    """
+    Reads a PDF file and returns its text content.
+    """
+    text = ""
+    try:
+        pdf_document = pymupdf.open(pdf_path) # Open the PDF file
+        # loop through text, print below for testing
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document.load_page(page_num)
+            text += page.get_text()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return text
 
-# loop through text, print below for testing
-for page_num in range(pdf_document.page_count):
-    page = pdf_document.load_page(page_num)
-    text = page.get_text()
-    print(text)
+
