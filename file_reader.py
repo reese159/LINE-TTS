86948ff69422c74ae46a0a7b8f55dc1642bed542
Reese Pelletier <reese15910@gmail.com>
2025-07-12 14:39:54 -0400

    Completed function to handle text cropping

--- file_reader.py
+++ file_reader.py
@@ -10,28 +10,33 @@ def read_pdf(pdf_path):
         pdf_document = fitz.open(pdf_path) # Open the PDF file
         # loop through text, print below for testing
         for page_num in range(pdf_document.page_count):
-            page = pdf_document.load_page(page_num)
-            
-            #===== removing headers and footers
-            header_height_percent = 0.05
-            footer_height_percent = 0.05
-            
-            page_height = page.rect.height
-            header_cutoff = page_height * header_height_percent
-            footer_cutoff = page_height * (1 - footer_height_percent)
-            
-            page_shape = page.rect
-            
-            # Set new page shape to exclude header and footer
-            cropped_page = fitz.Rect(page_shape.x0, header_cutoff, page_shape.x1, footer_cutoff)
-            page.set_cropbox(cropped_page)
-            
-            #===== removing headers and footers
-            
-            text += page.get_text()
-            
+            text += cropped_text(pdf_document, page_num) # Read each page and crop text
+        
         pdf_document.close() # Close the document after reading
     except Exception as e:
         print(f"An error occurred: {e}")
     return text.strip()
 
+
+def cropped_text(pdf_document, page_num):
+    """
+    Crops header and footer from current page in PDF, 
+    returns text after reading
+    """
+    page = pdf_document.load_page(page_num)
+    
+    # Set header and footer percentages
+    header_height_percent = 0.05
+    footer_height_percent = 0.05
+    
+    # Find new page dimensions
+    page_height = page.rect.height
+    header_cutoff = page_height * header_height_percent
+    footer_cutoff = page_height * (1 - footer_height_percent)
+    page_shape = page.rect
+    
+    # Set new page shape to exclude header and footer
+    cropped_page = fitz.Rect(page_shape.x0, header_cutoff, page_shape.x1, footer_cutoff)
+    page.set_cropbox(cropped_page)
+    
+    return page.get_text()
