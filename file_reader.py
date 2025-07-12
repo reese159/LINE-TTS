857b83ebdb575ba340d4133f77bd64fdc5c17aeb
Reese Pelletier <reese15910@gmail.com>
2025-07-12 14:21:21 -0400

    File reader removing header and footer - WIP

--- file_reader.py
+++ file_reader.py
@@ -1,17 +1,37 @@
-import pymupdf
+import fitz  
 
 def read_pdf(pdf_path):
     """
     Reads a PDF file and returns its text content.
     """
     text = ""
+    
     try:
-        pdf_document = pymupdf.open(pdf_path) # Open the PDF file
+        pdf_document = fitz.open(pdf_path) # Open the PDF file
         # loop through text, print below for testing
         for page_num in range(pdf_document.page_count):
             page = pdf_document.load_page(page_num)
+            
+            #===== removing headers and footers
+            header_height_percent = 0.05
+            footer_height_percent = 0.05
+            
+            page_height = page.rect.height
+            header_cutoff = page_height * header_height_percent
+            footer_cutoff = page_height * (1 - footer_height_percent)
+            
+            page_shape = page.rect
+            
+            # Set new page shape to exclude header and footer
+            cropped_page = fitz.Rect(page_shape.x0, header_cutoff, page_shape.x1, footer_cutoff)
+            page.set_cropbox(cropped_page)
+            
+            #===== removing headers and footers
+            
             text += page.get_text()
+            
+        pdf_document.close() # Close the document after reading
     except Exception as e:
         print(f"An error occurred: {e}")
-    return text
+    return text.strip()
 
