8d775e548cd7831a4ac3b7c69d1e9b0f5fe3c9f0
Reese Pelletier <reese15910@gmail.com>
2025-07-20 12:30:19 -0400

    refatoring summarization, new models & streamlit layout

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -3,6 +3,7 @@ import torch
 import soundfile as sf
 import audio_joiner as aj
 import voice_blend
+import file_reader
 import io
 from IPython.display import display, Audio
 from pydub import AudioSegment
@@ -15,8 +16,8 @@ st.write("Local Interactive Narration Environment")
 # Initialize the validation checks
 if 'valid_voice' not in st.session_state:
     st.session_state.valid_voice = False
-if 'valid_text' not in st.session_state:        # TODO: Validate current text input prior to summary & narration
-    st.session_state.valid_text = False
+if 'text_input' not in st.session_state:        # TODO: Validate current text input prior to summary & narration
+    st.session_state.text_input = ""
 
 # Initialize voices in session state
 if "voices" not in st.session_state:
@@ -28,7 +29,7 @@ with st.sidebar:
     st.subheader("Config")
     st.write("Adjust the settings below:")
     # Option to change OpenAI model
-    model = st.selectbox("Select OpenAI Model", ["gpt-3.5-turbo", "gpt-4"], index=0)
+    model = st.selectbox("Select OpenAI Model", ["gpt-4o-mini", "gpt-4.1-nano", "gpt-4.1"], index=0)
     # Option to set max tokens for summary
     max_tokens = st.slider("Max Tokens for Summary Output", min_value=50, max_value=1000, value=250, step=50)
     st.write("Credit to hexgrad for Kokoro-82M voice models and Kokoro inference library")
@@ -73,8 +74,6 @@ if st.session_state.voices:
         )
     # Update the weight in the session state
         st.session_state.voices[i]["weight"] = weight
-        print(st.session_state.voices)
-        print(st.session_state.voices[i]["weight"])
         current_weights.append(weight)
         
     # Validate sum of weights to be 1.0
@@ -92,43 +91,46 @@ input_type = st.radio("Choose Input Type:", ("Upload PDF", "Enter Text"))
 if input_type == "Upload PDF":
     uploaded_file = st.file_uploader("Upload your PDF", type="pdf")
     if uploaded_file is not None:
+        st.session_state.text_input = file_reader.read_pdf(uploaded_file)  # Read the PDF content
         st.success("PDF file uploaded successfully!")
-        # TODO: read through pdf
 elif input_type == "Enter Text":
-    text_input = st.text_area("Enter your text here:", height=150)
-    if text_input:
+    st.session_state.text_input = st.text_area("Enter your text here:", height=150)
+    if st.session_state.text_input:
         st.success("Text entered successfully!")
 
 # --- Generate summary ---
 from text_summarization import summarize_text
-st.subheader("Text Summarization")
 
-if st.button("Summarize Text"):
-    log_narration=""
-    narration_text_box = st.empty()
-    
-    summary = summarize_text(text_input, model=model, max_tokens=250, openai_api_key=st.secrets["OPENAI_API_KEY"])
-    st.write("Summary:")
-    st.write(summary)
-    
-    current_voices = [voice["tensor"] for voice in st.session_state.voices]
-    current_weights = [voice["weight"] for voice in st.session_state.voices]
-    new_pipeline, new_voice = voice_blend.blending_pt_files(current_voices, current_weights, summary)
-    summary_audio = AudioSegment.empty()
-    
-    # narration_text_box.text_area("Watch the narration process:", "", height=150)
-    
-    # display and save audio segments using method displayed in kokoro documentation:
-    for i, (gs, ps, audio) in enumerate(new_pipeline):
-        log_narration +=  f"Segment {i}: \n"  # i => index
-        log_narration += f"Graphemes: {gs} \n"  # gs => graphemes/text
-        log_narration += f"Phonemes: {ps} \n"  # ps => phonemes
-        narration_text_box.text_area("Watch the narration process:", log_narration, height=150)
-        new_audio_segment  = aj.tensor_to_audio_segment(audio, sample_rate=24000)
-        summary_audio += new_audio_segment
-        print(log_narration)
-        # st.audio #add_audio_to_narration(temp_path=NotImplemented, narration_name="user_narration.wav", new_audio=audio)
-    audio_buffer = io.BytesIO()
-    summary_audio.export(audio_buffer, format="wav") 
-    st.audio(data=audio_buffer)
-    
+if st.session_state.text_input:
+    st.subheader("Text Summarization")
+
+    if st.button("Summarize Text"):
+        log_narration=""
+        
+        with st.spinner("Generating summary..."):
+            summary = summarize_text(st.session_state.text_input, model=model, max_tokens=max_tokens, openai_api_key=st.secrets["OPENAI_API_KEY"])
+            st.text_area("Sumary:", summary, height=150)
+            narration_text_box = st.empty()
+            
+            
+            current_voices = [voice["tensor"] for voice in st.session_state.voices]
+            current_weights = [voice["weight"] for voice in st.session_state.voices]
+            new_pipeline, new_voice = voice_blend.blending_pt_files(current_voices, current_weights, summary)
+            summary_audio = AudioSegment.empty()
+            
+            # narration_text_box.text_area("Watch the narration process:", "", height=150)
+            
+            # display and save audio segments using method displayed in kokoro documentation:
+            for i, (gs, ps, audio) in enumerate(new_pipeline):
+                log_narration +=  f"Segment {i}: \n"  # i => index
+                log_narration += f"Graphemes: {gs} \n"  # gs => graphemes/text
+                log_narration += f"Phonemes: {ps} \n"  # ps => phonemes
+                narration_text_box.text_area("Watch the narration process:", log_narration, height=150)
+                new_audio_segment  = aj.tensor_to_audio_segment(audio, sample_rate=24000)
+                summary_audio += new_audio_segment
+                print(log_narration)
+                # st.audio #add_audio_to_narration(temp_path=NotImplemented, narration_name="user_narration.wav", new_audio=audio)
+            audio_buffer = io.BytesIO()
+            summary_audio.export(audio_buffer, format="wav") 
+            st.audio(data=audio_buffer)
+        
